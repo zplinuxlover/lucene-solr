47f656d625027c45f30523129cc1dc3337b0146f
Cassandra Targett <cassandra.targett@lucidworks.com>
2019-12-07 08:28:12 -0600

    WIP redesign of Ref Guide layout: bootstrap upgrade, replace TOC functions

--- solr/solr-ref-guide/src/js/customscripts.js
+++ solr/solr-ref-guide/src/js/customscripts.js
@@ -1,5 +1,5 @@
 
-$('#mysidebar').height($(".nav").height());
+//$('#sidebar').height($(".nav").height());
 
 
 $( document ).ready(function() {
@@ -100,4 +100,23 @@ $(function() {
       return $this.find("a[data-toggle=tab]:first, a[data-toggle=pill]:first").tab("show");
     }
   });
+});
+
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
 });
